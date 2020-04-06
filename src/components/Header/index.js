0574c9ba671ad86c3283d98a8b7e432d234cf117
Everton747 <everton_747@hotmail.com>
2020-04-06 13:55:06 -0300

    add index page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import './styles.scss';
-
 import logoImg from '../../assets/logo.png';
 
 export default function Header() {
--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdKeyboardArrowRight } from 'react-icons/md';
+
+import './styles.scss';
+import driverShape from '../../assets/driverShape.png';
 
 import Header from '../../components/Header';
 
-import './styles.scss';
-
 export default function Index() {
   return (
-    <Header></Header>
+    <div>
+      <Header />
+      <div className="index-container">
+        <button>Adicionar</button>
+        <ul>
+          <li>
+            <img src={driverShape} alt="Foto Motorista" />
+            <section>
+              <p>Rosimeire Fernandes Tabosa Vanzela</p>
+              <p>653.617.171-34</p>
+            </section>
+            <Link className="button-to-profile" to="/Edit">
+              <MdKeyboardArrowRight size={45} color="#000" className="icon"></MdKeyboardArrowRight>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
