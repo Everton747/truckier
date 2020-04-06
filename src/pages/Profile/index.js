6998be6d357976cd3a6310969c930657793ed2d1
Everton747 <everton_747@hotmail.com>
2020-04-06 15:58:39 -0300

    add mask in inputs

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import StringFormatValidation from 'string-format-validation'
 import { MdKeyboardArrowLeft } from 'react-icons/md';
 
 import './styles.scss';
@@ -7,6 +8,17 @@ import './styles.scss';
 import Header from '../../components/Header';
 
 export default function Edit(props) {
+
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [birthDate, setBirthDate] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [cnh, setCnh] = useState('');
+  const [cnhType, setCnhType] = useState('');
+
+  useEffect(() => { setPhone(StringFormatValidation.format('(##) # ####-####', phone)); }, [phone]);
+  useEffect(() => { setBirthDate(StringFormatValidation.format('##/##/####', birthDate)); }, [birthDate]);
+  useEffect(() => { setCpf(StringFormatValidation.format('###.###.###-##', cpf)); }, [cpf]);
 
   const history = useHistory();
 
@@ -20,18 +32,17 @@ export default function Edit(props) {
             <MdKeyboardArrowLeft size={30} color="#000" />
             <div>Voltar</div>
           </div>
-          <div className="container-name">{props.driver ? 'Editar Motorista': 'Novo Motorista'}</div>
+          <div className="container-name">{props.driver ? 'Editar Motorista' : 'Novo Motorista'}</div>
         </div>
 
         <form>
-          <input placeholder="Nome" />
-          <input placeholder="Telefone" />
-          <input placeholder="Data de nascimento" />
-          <input placeholder="CPF" />
-          <input placeholder="Nome" />
+          <input placeholder="Nome" value={name} onChange={e => setName(e.target.value)} />
+          <input placeholder="Telefone" value={phone} onChange={e => setPhone(e.target.value)} />
+          <input placeholder="Data de nascimento" value={birthDate} onChange={e => setBirthDate(e.target.value)} />
+          <input placeholder="CPF" value={cpf} onChange={e => setCpf(e.target.value)} />
           <div className="input-group">
-            <input placeholder="CNH" />
-            <input placeholder="Tipo" />
+            <input placeholder="CNH" value={cnh} onChange={e => setCnh(e.target.value)} />
+            <input placeholder="Tipo" value={cnhType} onChange={e => setCnhType(e.target.value)} />
           </div>
           <button>Cadastrar</button>
         </form>
