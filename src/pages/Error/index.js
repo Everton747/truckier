33067f13b618233e9211cfbee12f5e5f203314ad
Everton747 <everton_747@hotmail.com>
2020-04-06 00:41:09 -0300

    Error Page

---
+++ src/pages/Error/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdArrowBack } from 'react-icons/md';
+
+
+import './styles.scss';
+
+import logoImg from '../../assets/logo.png'
+
+export default function Error() {
+  return (
+    <div className="error-container">
+      <img src={logoImg} alt="logo" />
+      <h1 className="error-code">404</h1>
+      <h3 className="error-message">Página não encontrada</h3>
+      <Link className="back-link" to="/">
+        <MdArrowBack className="back-icon" size={25} color="#BE1E2D"/>
+        Página Inicial
+      </Link>
+    </div>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Index from './pages/Index';
+import Error from './pages/Error';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Index}></Route>
+        <Route component={Error}></Route>
       </Switch>
     </BrowserRouter>
   );
